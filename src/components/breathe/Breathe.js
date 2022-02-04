7df6b37fb527ef6f5bf2ed3f54e30d58337fb2d0
qapush <mihail.kapush@gmail.com>
2022-02-05 00:44:59 +0100

    Minor changes

--- src/components/breathe/Breathe.js
+++ src/components/breathe/Breathe.js
@@ -1,4 +1,3 @@
-import { useReducer } from "react";
 import { useSpring, animated } from "@react-spring/web";
 import Particles from "react-tsparticles";
 import ptOptions from "../../pt_options.json";
@@ -6,31 +5,24 @@ import "./breathe.scss";
 
 export default function Breathe({ params }) {
   const { inhale, pause, exhale, bgColor } = params;
-  const duration = inhale + pause + exhale;
 
-
-
-  const [ spring] = useSpring(() => ({
-    from: { transform: 'scale(0.01)'},
+  const [spring] = useSpring(() => ({
+    from: { transform: "scale(0.01)" },
     to: [
-      { transform: 'scale(1)', config: {duration: inhale * 1000} },
-      { transform: 'scale(0)', config: { duration: exhale * 1000 }, delay: pause * 1000 }
+      { transform: "scale(1)", config: { duration: inhale * 1000 } },
+      {
+        transform: "scale(0)",
+        config: { duration: exhale * 1000 },
+        delay: pause * 1000,
+      },
     ],
-    loop: { reset: true }
-  }))
-
-
+    loop: { reset: true },
+  }));
 
   return (
     <div className="breathe" style={{ backgroundColor: bgColor }}>
       <div className="breathe__inner">
-{/* 
-        <h3>
-          Stage
-        </h3> */}
-
         <animated.div className="bInner" style={spring}></animated.div>
-        
       </div>
       <Particles params={ptOptions} className="ptWrapper" />
     </div>
--- src/components/spring/Spring.js
+++
@@ -1,8 +0,0 @@
-import { animated, useSpring } from '@react-spring/web'
-
-function Spring() {
-  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
-  return <animated.div style={props}>I will fade in</animated.div>
-}
-
-export default Spring;
